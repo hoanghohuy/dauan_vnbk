e522c14148cfe08503881b21586d8524f065ad61
VuCongThuong <congthuong147@gmail.com>
2024-02-29 16:01:37 +0700

    update API

--- src/app/api/exam/[id]/route.js
+++ src/app/api/exam/[id]/route.js
@@ -8,7 +8,7 @@ export const GET = async (request, { params }) => {
   try {
     const user = await User.findById(id);
     if (!user) {
-      return NextResponse.json({error: "User not found"}, { status: 404 });
+      return NextResponse.json({error: "Exam not found"}, { status: 404 });
     }
 
     return NextResponse.json(user);
@@ -19,10 +19,10 @@ export const GET = async (request, { params }) => {
 
 export const PUT = async (request, { params }) => {
   const { id } = params;
-  const updateData = await request.json();
+  const {published, points} = await request.json();
 
   try {
-    const response = await User.findOneAndUpdate({_id: id}, updateData);
+    const response = await User.findOneAndUpdate({_id: id}, { published, points });
     return NextResponse.json(response);
   } catch (error) {
     return NextResponse.json({error:error.message}, { status: 500 });
