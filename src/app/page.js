fae157acfab7c2730a53ae047f77a19507b272a7
hhhoang.h3@gmail.com <hhhoang.h3@gmail.com>
2024-03-16 08:29:06 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -13,6 +13,19 @@ export const metadata = {
   title: "Cuộc thi Dấu ấn trên từng bước chân - Vietnambooking Event",
   description:
     "Hãy chia sẻ những kỷ niệm tại một vùng đất trên thế giới mà bạn đã đi qua! Trong đó ghi lại cảm nhận của bạn về cảnh đẹp, con người, văn hóa, hoạt động thú vị,... để truyền cảm hứng, thu hút bạn đọc đến vùng đất này.",
+  alternates: {
+    canonical: "https://event.vietnambooking.com",
+  },
+  openGraph: {
+    title: "Cuộc thi Dấu ấn trên từng bước chân - Vietnambooking Event",
+    url: "https://event.vietnambooking.com",
+    type: "website",
+    description:
+      "Hãy chia sẻ những kỷ niệm tại một vùng đất trên thế giới mà bạn đã đi qua! Trong đó ghi lại cảm nhận của bạn về cảnh đẹp, con người, văn hóa, hoạt động thú vị,... để truyền cảm hứng, thu hút bạn đọc đến vùng đất này.",
+    images: [`https://event.vietnambooking.com/banner_thele.png`],
+    site: "@vnbk",
+    creator: "@vnbk",
+  },
 };
 
 export default function Home() {
